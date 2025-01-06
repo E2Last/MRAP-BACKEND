561a49d71061725e7acf0c4f0b6b8a1d9caefb67
Esteban Gómez <65051066+E2Last@users.noreply.github.com>
2025-01-06 11:53:59 -0300

    .

--- MRAP BACKEND V2/build.sh
+++ MRAP BACKEND V2/build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-set -o errexit
+set -o errexit  # Salir inmediatamente si un comando falla
 
+# Moverse al directorio donde se encuentra manage.py
+cd "MRAP BACKEND V2"
+
+# Instalar dependencias
 pip install -r requirements.txt
 
+# Ejecutar comandos de Django
 python manage.py collectstatic --noinput
-python manage.py migrate 
+python manage.py migrate
